b1ac2049be4ce6895950c1e8df4959903814eb0c
Jaren Cudilla <jarencudilla@gmail.com>
2021-02-19 23:03:33 +0800

    created JS file

---
+++ Javascript Activities/BMI.js
@@ -0,0 +1,13 @@
+var vheight = prompt("Victor's Height: (in Meters)");
+var vmass = prompt("Victor's Weight:");
+var jheight = prompt("John's Height: (in Meters)");
+var jmass = prompt("John's Weight:");
+
+bmi_victor = vmass / (vheight * vheight);
+console.log (bmi_victor);
+bmi_john = jmass / (jheight * jheight);
+console.log (bmi_john);
+
+var comp = bmi_victor > bmi_john;
+console.log ("Is Victor's BMI higher than John's?" + comp );
+
