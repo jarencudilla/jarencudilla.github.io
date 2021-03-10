3386c681223f2b28598e6ce7e0ba50d6647e8a73
Jaren Cudilla <jarencudilla@gmail.com>
2021-03-10 18:22:59 +0800

    Create chess.js
    New activity -
    
    -add legal moves for all pieces
    -collision detection
    -castling (king rook only)
    -en passant
    -pawn promotion

---
+++ JS-Activities/Chess/chess.js
@@ -0,0 +1,17 @@
+function allowDrop(allowdropevent) {
+    allowdropevent.preventDefault();
+}
+
+function drag(dragevent) {
+    dragevent.dataTransfer.setData("text", dragevent.target.id);
+}
+
+function drop(dropevent) {
+    dropevent.preventDefault();
+    var data = dropevent.dataTransfer.getData("text");
+    dropevent.target.appendChild(document.getElementById(data));
+}
+/* to do 
+legal moves
+collision detection
+*/
