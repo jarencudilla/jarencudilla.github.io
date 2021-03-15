485f4f32107366d52720fc84c80375743216978f
Jaren Cudilla <jarencudilla@gmail.com>
2021-03-15 20:36:13 +0800

    Update chess.js

--- JS-Activities/Chess/chess.js
+++ JS-Activities/Chess/chess.js
@@ -15,3 +15,33 @@ function drop(dropevent) {
 legal moves
 collision detection
 */
+
+
+// function checkPiece() {
+// document.getElementsByClassName('ebony ivory');
+// document.addEventListener('onclick', function(event)){
+//     var pawn = 
+// }
+
+
+
+// to check what is being moved/clicked
+var lastClicked;
+function check(id) {
+    var src,dest;
+    if (!lastClicked) {
+        lastClicked = id;
+    } else {
+        src = document.getElementById(lastClicked);
+        dest =document.getElementById(id);
+        dest.value = src.value;
+        src.value = "";
+        lastClicked = null;
+    }
+}
+
+// an onclick event for every div 
+
+var state = false
+
+var cells = document.getElementById('#a1', '#a2', '#a3', '#a4', '#a5', '#a6', '#a7', '#a8',)
